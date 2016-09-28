3bda1eaaf49b40729cd096a3ae73a1dabf9ca9a2
gokulkreshna <blassreiter21@gmail.com>
2016-09-28 16:51:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ element.innerHTML ="NEW VALUE";
 //move the image
 
 var img= document.getElementById("madi");
-img.onclick = function () {
+img.onclick = function() {
    img.style.marginleft = "100px";
 };
