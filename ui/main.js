9933391f99c93929cfad29c7f3a8cd4988a84198
gokulkreshna <blassreiter21@gmail.com>
2016-10-03 22:30:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick = function () {
     
     };
     //Make the request
-    request.open('GET','http://gokulkreshna.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://gokulkreshna.imad.hasura-app.io/counter', true);
     request.send(null);
     };
 
