6a81cc1c0c020402f560bef50bd22e07d2b7c8f2
gokulkreshna <blassreiter21@gmail.com>
2016-10-03 23:12:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,17 @@
 var button = document.getElementById('counter');
 var counter = 0;
 
+button.onclick = function () {
+   //Make a request
+   var request = new XMLHttpRequest();
+   
+   //capture the response and store it in a variable
+   
+   //render the response in correct span
+       counter = counter + 1;
+       var span = document.getElementById('count');
+       span.innerHTML = counter.toSring();
+};
+
 
 
