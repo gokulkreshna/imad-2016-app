cdfe563466678ce777fec66da36937ca2fea9a68
gokulkreshna <blassreiter21@gmail.com>
2016-10-06 20:31:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function() {
         request.send(null);
     };
 
-
+// submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit');
@@ -34,7 +34,7 @@ submit.onclick = function () {
   //capture a list of names and render it as a list
    var names = ['name1','name2','name3','name4'];
    var list = '';
-   for (vari=0;i<names.length;i++) {
+   for (var i=0; i< names.length; i++) {
        list += '<li>' + names[i] + '<li>';
    }
    var ul = document.getElementById('namelist');
