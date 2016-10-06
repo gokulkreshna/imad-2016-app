eb368edb8b9372c57931d8109f314d4542922e79
gokulkreshna <blassreiter21@gmail.com>
2016-10-06 19:17:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,18 @@
+// Counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
+button.onclick = function() {
+    
+    // Create a request
+    
+    // Capture the response and store it in a variable
+    
+    //Render the response in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
 
 
