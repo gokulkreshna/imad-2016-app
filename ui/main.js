c6e876e86c9a59af59da70769e159ee5212d3144
gokulkreshna <blassreiter21@gmail.com>
2016-10-06 19:30:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,15 +4,24 @@ var counter = 0;
 
 button.onclick = function() {
     
-    // Create a request
+    // Create a request object
+    var request = new XMLHttpRequest();
     
     // Capture the response and store it in a variable
-    
-    //Render the response in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-};
+        request.onreadystatechange = function () {
+          if (request.readystate === XMLHttpRequest.DONE) {
+                // take some action
+                if (request.status === 200) {
+                    var counter = request.responseText;
+                     var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                }
+                  }
+            
+             };
+        // MAke the request
+        request.open('GET', 'http://gokulkreshna.imad.hasura-app.io/counter', true);
+        request.send(null);
+    };
 
 
